e863497b8dbebabd05913259e90d7a195ec9ce3f
widhipranata35 <62409202+widhipranata35@users.noreply.github.com>
2020-11-06 12:23:53 +0700

    Tugas Akhir
    Ending dari semua ini

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -4,48 +4,83 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-int quantity = 0;
-
+    int jumlah = 0;
+    EditText namaEditText;
+    String nama;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
     public void increment(View view) {
-        quantity = quantity + 1;
-        display(quantity);
+        jumlah = jumlah + 1;
+        display(jumlah);
     }
 
     public void decrement(View view) {
-        quantity = quantity - 1;
-        display(quantity);
+        if (jumlah > 0 == false) {
+        } else {
+            jumlah = jumlah - 1;
+            display(jumlah);
+        }
     }
+    public void submitOrder(View view) {
+        CheckBox iwatchphoneCheckbox = (CheckBox) findViewById(R.id.iwatchphone_checkbox);
+        CheckBox earphoneCheckbox = (CheckBox) findViewById(R.id.earphone_checkbox);
+        namaEditText = (EditText) findViewById(R.id.edit_nama);
 
-    public void submitOrder (View view) {
-        int price = quantity * 5000;
-        String priceMessage = "Total Pembelian RP " + price;
-        priceMessage = priceMessage + "\nTerima Kasih !";
+        boolean hasondeonde = iwatchphoneCheckbox.isChecked();
+        boolean hasmolen = earphoneCheckbox.isChecked();
+        nama = namaEditText.getText().toString();
+
+        int price = calculatePrice(hasondeonde, hasmolen);
+
+        String priceMessage = "Apa Tambah Lagi?? ...\n";
+        priceMessage = priceMessage + "\nBeli : "+ jumlah + " Unit" +
+                "\nIwatchphone: " + hasondeonde +
+                "\nEarphone: " + hasmolen +
+                "\nTotal Pembelian Rp: "+ price +
+                "\n\nTerima Kasih, " + nama;
+
         displayMessage(priceMessage);
     }
 
+    private int calculatePrice(boolean addhasiwatchphone, boolean addhasearphone) {
+        int hargaawal = 1000000;
+        int harga = 0;
+
+        if(addhasiwatchphone){
+            hargaawal = hargaawal + 600000;
+        }
+        if(addhasearphone){
+            hargaawal = hargaawal + 200000;
+        }
+        return harga = jumlah * hargaawal;
+    }
+
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.harga_text_view);
         priceTextView.setText(message);
     }
 
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        TextView jumlahTextView = (TextView) findViewById(R.id.jumlah_text_view);
+        jumlahTextView.setText("" + number + " unit");
     }
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    private void displayHarga(int number){
+        Locale localeID = new Locale("in", "ID");
+        NumberFormat formatRupiah = NumberFormat.getCurrencyInstance(localeID);
+        TextView hargaTextView = (TextView) findViewById(R.id.harga_text_view);
+        hargaTextView.setText(formatRupiah.format(number));
     }
 }
